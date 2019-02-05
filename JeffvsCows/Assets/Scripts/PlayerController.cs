99e7d286dd1fe576cb39e7eef64da10254b3a800
arthurshime3 <arthur.shim.e3@gmail.com>
2019-02-04 22:38:57 -0500

    beginning of roll

--- JeffvsCows/Assets/Scripts/PlayerController.cs
+++ JeffvsCows/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,21 @@ public class PlayerController : MonoBehaviour
     BoxCollider collider;
 
     public float gravity = -1.0f;
-    public float moveSpeed = 2;
+    public float moveSpeed = 2f, rollSpeed = 4f;
     Vector3 velocity;
+
+    bool rolling;
+    float rollTime = 0.5f, rollTimeRemaining;
+    Vector3 rollDirection;
 
     void Start()
     {
         controller = GetComponent<Controller3D>();
         collider = GetComponent<BoxCollider>();
-
+        rolling = false;
         velocity = new Vector3();
     }
-
-
+    
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -31,7 +34,66 @@ public class PlayerController : MonoBehaviour
 
         velocity.z = input.y * moveSpeed;
 
+        if (velocity.magnitude > moveSpeed)
+        {
+            velocity = velocity.normalized * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Debug.Log("shift pressed");
+            if (!rolling)
+            {
+                rolling = true;
+                rollTimeRemaining = rollTime;
+                rollDirection = GetFaceDir();
+                Debug.Log(rolling);
+            }
+        }
+
+        if (rolling)
+        {
+            velocity = rollDirection * rollSpeed;
+            rollTimeRemaining -= Time.deltaTime;
+            rolling = rollTimeRemaining > 0f;
+        }
+
         velocity.y += gravity * Time.deltaTime;
+        Debug.Log(rollDirection);
+        //Debug.Log(rolling);
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    Vector3 GetFaceDir()
+    {
+        Vector3 direction = new Vector3();
+        switch(controller.collisions.faceDir)
+        {
+            case 0:
+                direction = new Vector3(0, 0, 1);
+                break;
+            case 1:
+                direction = new Vector3(0.5f, 0, 0.5f);
+                break;
+            case 2:
+                direction = new Vector3(1, 0, 0);
+                break;
+            case 3:
+                direction = new Vector3(0.5f, 0, -0.5f);
+                break;
+            case 4:
+                direction = new Vector3(0, 0, -1);
+                break;
+            case 5:
+                direction = new Vector3(-0.5f, 0, -0.5f);
+                break;
+            case 6:
+                direction = new Vector3(-1, 0, 0);
+                break;
+            case 7:
+                direction = new Vector3(-0.5f, 0, 0.5f);
+                break;
+        }
+        return direction;
     }
 }
