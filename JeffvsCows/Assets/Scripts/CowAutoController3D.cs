722dc676767e0291f27e55b6abf2bf1e779b0b1d
calebrudnicki <calebrudnicki@gmail.com>
2019-04-09 23:42:50 -0400

    Worked up to cow automation

---
+++ JeffvsCows/Assets/Scripts/CowAutoController3D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CowAutoController3D : MonoBehaviour
+{
+
+    public Transform[] target;
+    public float speed;
+    private int current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position) 
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        } else current = (current + 1) % target.Length;
+
+    }
+}
